a470682134cf8ab9283bfe5fd4eee303bc8804e7
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-18 22:44:31 +0300

    Added task 3

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,11 +4,22 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 # 2nd task
+
 from django.shortcuts import redirect
-from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
+
+
+#3rd 
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from .models import UserProfile
+
+
+
+
+
 
 #1st
 # Function-based 
@@ -40,3 +51,30 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+    
+    
+    
+# 3rd
+def user_is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def user_is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def user_is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(user_is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(user_is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(user_is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')    
