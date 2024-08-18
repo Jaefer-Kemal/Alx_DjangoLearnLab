256b75307aa8b302e96dfd7ac3e251e4ba4d909a
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-18 22:32:41 +0300

    Added task 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+#1st
 from .views import list_books, LibraryDetailView
+#2nd
+from .views import register, LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,16 @@
+#1st task
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+# 2nd task
+from django.shortcuts import redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
+#1st
 # Function-based 
 def list_books(request):
     books = Book.objects.all()
@@ -13,3 +21,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+#2nd
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+class LoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class LogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
