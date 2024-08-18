f92697dc0506d394fa636a55428aae29ec872b11
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-18 23:15:47 +0300

    Modified the urls for 4th task

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import list_books, LibraryDetailView
 #2nd
 from .views import LoginView, LogoutView
 from . import views
-from .views import admin_view, librarian_view, member_view, register
+from .views import admin_view, librarian_view, member_view, RegisterView
 #3rd
 from django.urls import path
 from .views import add_book, edit_book, delete_book
@@ -12,17 +12,15 @@ from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     # Existing URL patterns
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+     path('', list_books, name='book_list'),  # Adjust the path if needed
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-
-    # Secured views for books
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
