6c7cdbc519bd3511b3f7492f221435a7616a3daf
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-18 22:52:09 +0300

    modified models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 #3rd
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from django.dispatch import receiver
+
+
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -40,17 +41,16 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f'{self.user.username} - {self.role}'
+        return f"{self.user.username} - {self.role}"
 
-@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+
+post_save.connect(create_user_profile, sender=User)
