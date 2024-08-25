7a6f08f4c0b74c0b561bb7da2c2523cd22b63ed5
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-25 21:16:07 +0300

    Added permission to the Book Model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from .models import Book, CustomUser
+from .models import Book,  CustomUser
 from django.contrib.auth.admin import UserAdmin
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
+    list_display = ('title', 'author', 'published_date')  # Corrected field name
+    list_filter = ('author', 'published_date')
     search_fields = ('title', 'author')
     
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        (None, {"fields": ("birth_date",)}),
+        (None, {"fields": ("date_of_birth",)}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -14,7 +14,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("User must have an email")
 
         user = self.model(email=self.normalize_email(email))
-        user.setpassword(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -30,3 +30,12 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+    
+    class Meta:
+        permissions = [
+            ("can_create", "Can create Books"),
+            ("can_view", "Can read books"),
+            ("can_edit","Can edit books"),
+            ("can_delete","Can delete books")
+        ]
+
