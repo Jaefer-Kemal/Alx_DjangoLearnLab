e9bee4d48f60dc7376ad757dddc77d5012cfb4b9
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-01 22:20:17 +0300

    Added autentication and permission in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,14 +4,15 @@ import rest_framework.generics
 import rest_framework.viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+    permission_classes = [IsAdminUser]
     
 class BookViewSet(rest_framework.viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+    permission_classes = [IsAuthenticated]
     
