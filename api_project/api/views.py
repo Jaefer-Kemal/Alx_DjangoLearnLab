75e66d26b82a042907f277f2483b5cfe26df8da5
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-01 21:12:21 +0300

    Added a new view for Book but using ModelViewset instead of ListApiView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 import rest_framework
 import rest_framework.generics
+import rest_framework.viewsets
 from .serializers import BookSerializer
 from .models import Book
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    
+class BookViewSet(rest_framework.viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
     
