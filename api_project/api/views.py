fe8eec641b7fe9b31e6dfa52472d316f1d7779b1
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-01 21:08:08 +0300

    Added the seriliazer.py, views.pr and urls.py for api app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import rest_framework
+import rest_framework.generics
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+class BookList(rest_framework.generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
