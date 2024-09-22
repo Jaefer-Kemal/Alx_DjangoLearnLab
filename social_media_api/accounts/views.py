befa143f033b3d4a204d965a22bbaa11d6220931
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-22 19:56:04 +0300

    2nd Task has been implemented

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,12 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from .serializers import UserSerializer, LoginSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -34,3 +40,17 @@ class LoginView(APIView):
                 return Response({'token': token.key}, status=status.HTTP_200_OK)
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({'message': 'You are now following {}'.format(user_to_follow.username)})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({'message': 'You have unfollowed {}'.format(user_to_unfollow.username)})
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,11 @@ from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -18,3 +23,12 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_feed(request):
+    following_ids = request.user.following.values_list('id', flat=True)
+    posts = Post.objects.filter(author__id__in=following_ids).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)  # Ensure you have a PostSerializer
+    return Response(serializer.data)
