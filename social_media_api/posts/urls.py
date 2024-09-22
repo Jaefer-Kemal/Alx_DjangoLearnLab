19cd2318f0e2c911ddc622079a0c351c266151de
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-22 21:04:02 +0300

    Added a custom url for path

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -11,4 +11,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', user_feed, name='user_feed'),
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
 ]
