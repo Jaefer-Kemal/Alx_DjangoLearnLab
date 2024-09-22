b3b742bc617e9fb46a54a6554f23080862b96861
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-09-22 19:49:49 +0300

    Added the app for posts with all the requirement

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "posts",
     "rest_framework",
     'rest_framework.authtoken'
 ]
@@ -125,3 +126,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
